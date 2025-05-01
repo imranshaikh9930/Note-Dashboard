a7b360697be951b390bc517bb938c4b27f27b01d
Imran shaikh <imranpathan7040@gmail.com>
2025-05-01 16:32:26 +0530

    frontend url added

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3800;
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:5173', // or your deployed frontend URL
+    origin: 'https://note-dashboard-frontend-2.onrender.com', // or your deployed frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: "true", // if sending cookies
   }));
